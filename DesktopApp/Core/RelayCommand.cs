1d52cf2f6afd9f3c2a9b3bae35701888033e6d0c
s3822042 <s3822042@rmit.edu.vn>
2021-06-06 13:48:25 +0700

    add relay command core object

---
+++ DesktopApp/Core/RelayCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DesktopApp.Core
+{
+    class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Fun<object, bool> canExecute;
+
+        public event EventHandler CaneExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value;  }
+        }
+
+       
+    }
+}
