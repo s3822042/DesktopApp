766049c9ce878ecf58eac394059ef205eac51b15
s3822042 <s3822042@rmit.edu.vn>
2021-06-06 16:11:09 +0700

    finalise project

---
+++ DesktopApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DesktopApp.Core;
+
+namespace DesktopApp.MVVM.ViewModel
+{
+    class MainViewModel : OservableObject     
+    {
+        public HomeViewModel HomeVM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+    }
+}
